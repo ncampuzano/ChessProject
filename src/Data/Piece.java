7dde8e40c79cf3c911c797c4862ebbe3247ce2cc
Nicolás Campuzano <ncampuzanoa@unal.edu.co>
2015-11-25 22:46:32 -0500

    Getters and setters

--- src/Data/Piece.java
+++ src/Data/Piece.java
@@ -4,5 +4,30 @@ public class Piece {
 	public String PieceImage;
 	public boolean IsWhite;
 	public String Name;
-	public int Type;
+	public int Type;	
+	public String getPieceImage() {
+		return PieceImage;
+	}
+	public void setPieceImage(String pieceImage) {
+		PieceImage = pieceImage;
+	}
+	public boolean isIsWhite() {
+		return IsWhite;
+	}
+	public void setIsWhite(boolean isWhite) {
+		IsWhite = isWhite;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getType() {
+		return Type;
+	}
+	public void setType(int type) {
+		Type = type;
+	}
+	
 }
