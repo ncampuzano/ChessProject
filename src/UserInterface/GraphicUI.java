81eb64fcb4fd96f423ac83331ab7fd1bf24a6a72
Jmad12 <Juanenero@hotmail.com>
2015-11-28 22:32:04 -0500

    Changed the main process to distributed functions

--- src/UserInterface/GraphicUI.java
+++ src/UserInterface/GraphicUI.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -51,7 +52,7 @@ public class GraphicUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 500, 500);
+		frame.setBounds(100, 100, 750, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//Generate Main Panel, which contains the Tool Bar and the Game Panel
@@ -107,14 +108,25 @@ public class GraphicUI {
 		
 
 		//Instance the basic ChessBoard
+		chessBoardButtons = setBasicChessBoardButtons();
+		
+		//Generates the no-pieces ChessBoard
+		//Not needed, but useful for debugging
 		JPanel ChessBoard = setChessBoard(chessBoardButtons);
 		GamePanel.add(ChessBoard, BorderLayout.CENTER);
 		
 		initializeBoard();
 		
+		chessBoardButtons = paintPieces(pieces,chessBoardButtons);
+		ChessBoard = setChessBoard(chessBoardButtons);
+		GamePanel.add(ChessBoard, BorderLayout.CENTER);
+		
 	}
 	
-	//Instances all starting pieces and adds them to a list
+	/**
+	 *Generates the starting pieces, saving them into the ArrayList pieces
+	 *Completely unmanageable code, fix later
+	 */
 	public void initializeBoard(){
 		//White pieces
 		pieces.add(new Piece (true,1,1,1));
@@ -134,8 +146,8 @@ public class GraphicUI {
 		pieces.add(new Piece (false,1,8,1));
 		pieces.add(new Piece (false,2,8,2));
 		pieces.add(new Piece (false,3,8,3));
-		pieces.add(new Piece (false,4,8,4));
-		pieces.add(new Piece (false,5,8,5));
+		pieces.add(new Piece (false,5,8,4));
+		pieces.add(new Piece (false,4,8,5));
 		pieces.add(new Piece (false,3,8,6));
 		pieces.add(new Piece (false,2,8,7));
 		pieces.add(new Piece (false,1,8,8));
@@ -145,7 +157,51 @@ public class GraphicUI {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param pieces The list of pieces that must be painted
+	 * @param chessBoardButtons An already existing array of buttons to be modified
+	 * @return chessBoardButtons An array of buttons that has the painted pieces
+	 */
+	public JButton[][] paintPieces(List<Piece> pieces, JButton[][] chessBoardButtons){
+		for(Piece piece : pieces){
+			chessBoardButtons[8-(piece.getRow())][piece.getColumn()-1].setIcon(new ImageIcon(piece.getPieceImage())); 
+		}
+		return chessBoardButtons;
+		
+	}
 	
+	/**
+	 * Generates the basic background-colored array of buttons
+	 * @return an 8x8 array of buttons with just colored backgrounds
+	 */
+	public JButton[][] setBasicChessBoardButtons (){
+		//Generates an 8x8 Array of buttons and saves them 
+		for (int i = 0; i < 8; i++){
+			for (int j = 0; j < 8; j++){
+				JButton b = new JButton();
+				//Color each button the appropiate color
+				if((i+j)%2 == 0){
+					b.setBackground(Color.WHITE);
+				}
+				else{
+					b.setBackground(Color.GRAY);
+				}
+				b.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+					}
+				});
+				chessBoardButtons[i][j] = b;
+			}
+		}
+		return chessBoardButtons;
+	}
+	/**
+	 * Generates an entire ChessBoard , including a first row and first column of notation, and an 8x8 array of buttons
+	 * @param chessBoardButtons The buttons that must be shown
+	 * @return a ChessBoard with notation and the buttons
+	 */
 	public JPanel setChessBoard(JButton[][] chessBoardButtons){
 		//Generate a 9x9 JPanel (First row and column for notation (l to r :ABCDEFGH) (bot to top: 12345678)
 		JPanel ChessBoard = new JPanel();
@@ -157,25 +213,7 @@ public class GraphicUI {
 			ChessBoard.add( new JLabel( Character.toString( (char)i ) ,
                     SwingConstants.CENTER ) );
 		
-		//Generates an 8x8 Array of buttons and saves them 
-		for (int i = 0; i < 8; i++){
-			for (int j = 0; j < 8; j++){
-				JButton b = new JButton();
-				//Color each button the appropiate color
-				if((i+j)%2 == 0){
-					b.setBackground(Color.WHITE);
-				}
-				else{
-					b.setBackground(Color.BLACK);
-				}
-				b.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						
-					}
-				});
-				chessBoardButtons[i][j] = b;
-			}
-		}
+		
 		//Chooses between adding the leftmost numbers or introducing the 8x8 array of buttons to the main ChessBoard
 				for (int i = 0; i < 8; i++){
 					for (int j = 0; j < 8; j++){
