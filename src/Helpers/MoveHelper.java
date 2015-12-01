18168279919b2208b911e486a4f48db824829f34
Nicolás Campuzano <ncampuzanoa@unal.edu.co>
2015-11-30 20:05:37 -0500

    Functions in helper

--- src/Helpers/MoveHelper.java
+++ src/Helpers/MoveHelper.java
@@ -1,5 +1,56 @@
 package Helpers;
 
-public class MoveHelper {
+import Data.ChessGUI;
+import Data.Piece;
 
+public class MoveHelper {
+	private ChessGUI Chess;
+	
+	public MoveHelper(ChessGUI chess){
+		Chess = chess;		
+	}
+	
+	public void EnableMovements(int row, int col, int type){
+		
+		switch (type) {
+        case Piece.TYPE_BISHOP:
+        	EnableBishopMovements();
+            break;
+        case Piece.TYPE_KING:
+        	EnableKingMovements();
+            break;
+        case Piece.TYPE_KNIGHT:
+        	EnableKnightMovements();
+            break;
+        case Piece.TYPE_PAWN:
+        	EnablePawnMovements();
+            break;
+        case Piece.TYPE_QUEEN:
+        	EnableQueenMovements();
+            break;
+        case Piece.TYPE_ROOK:
+        	EnableRookMovements();
+            break;
+		}
+		
+	}
+	public void EnableRookMovements(){
+		
+	}
+	public void EnableQueenMovements(){
+		
+	}
+	public void EnablePawnMovements(){
+		
+	}
+	public void EnableKnightMovements(){
+		
+	}
+	public void EnableKingMovements(){
+		
+	}
+	public void EnableBishopMovements(){
+		
+	}
+	
 }
