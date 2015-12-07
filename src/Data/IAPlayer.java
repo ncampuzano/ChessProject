1bc81c212f8038935b661ba123a65e8876c615b5
Nicolás Campuzano <ncampuzanoa@unal.edu.co>
2015-12-07 18:48:08 -0500

    Implement new to class

---
+++ src/Data/IAPlayer.java
@@ -0,0 +1,134 @@
+package Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import UserInterface.GraphicUI;
+
+public class IAPlayer {
+	
+	private ChessGUI chessGame;
+	private	GraphicUI graphicUI;
+	
+    public IAPlayer(ChessGUI chessGame,GraphicUI graphic ) {
+        this.chessGame = chessGame;
+        this.graphicUI = graphic;
+    }
+    
+	private Move getBestMove() {
+ 
+        List<Move> validMoves = generateMoves();
+        int bestResult = Integer.MIN_VALUE;
+        Move bestMove = null;
+         
+        for (Move move : validMoves) {
+            executeMove(move);
+ 
+            int evaluationResult = this.evaluateState();
+ 
+            chessGame.undoMove(move);
+            if( evaluationResult > bestResult){
+                bestResult = evaluationResult;
+                bestMove = move;
+            }
+        }
+        System.out.println("done thinking! best move is: "+bestMove);
+        return bestMove;
+    }
+	 private List<Move> generateMoves() {
+		 
+	        List<Piece> pieces = this.chessGame.getPieces();
+	        List<Move> validMoves = new ArrayList<Move>();
+	        Move testMove = new Move(0,0,0,0);
+	 
+	        int pieceColor = this.chessGame.getGameState();
+	 
+	        // iterate over all non-captured pieces
+	        for (Piece piece : pieces) {
+	 
+	            // only look at pieces of current players color
+	            if (convertToInt(pieceColor) == piece.isWhite()) {
+	                // start generating move
+	                testMove.sourceRow = piece.getRow();
+	                testMove.sourceColumn = piece.getColumn();
+	 
+	                // iterate over all board rows and columns
+	                for (int targetRow = 0; targetRow <= 7; targetRow++) {
+	                    for (int targetColumn = 0 targetColumn <= 7; targetColumn++) {
+	 
+	                        // finish generating move
+	                        testMove.targetRow = targetRow;
+	                        testMove.targetColumn = targetColumn;
+	 
+	                        // check if generated move is valid
+	                        if (this.validator.isMoveValid(testMove, false)) {
+	                            // valid move
+	                            validMoves.add(testMove.clone());
+	                        } else {
+	                            // generated move is invalid, so we skip it
+	                        }
+	                    }
+	                }
+	 
+	            }
+	        }
+	        return validMoves;
+	    }
+	 private boolean convertToInt(int i){
+		 return (i == 1) ? true: false;
+	 }
+	 private int evaluateState() {
+	        // add up score
+	        //
+	        int scoreWhite = 0;
+	        int scoreBlack = 0;
+	        for (Piece piece : this.chessGame.getPieces()) {
+	            if(piece.getColor() == Piece.COLOR_BLACK){
+	                scoreBlack +=
+	                    getScoreForPieceType(piece.getType());
+	 
+	            }else if( piece.getColor() == Piece.COLOR_WHITE){
+	                scoreWhite +=
+	                    getScoreForPieceType(piece.getType());
+	 
+	            }else{
+	                throw new IllegalStateException(
+	                        "unknown piece color found: "+piece.getColor());
+	            }
+	        }
+	         
+	        // return evaluation result depending on who's turn it is
+	        int gameState = this.chessGame.getGameState();
+	         
+	        if( gameState == ChessGame.GAME_STATE_BLACK){
+	            return scoreBlack - scoreWhite;
+	         
+	        }else if(gameState == ChessGame.GAME_STATE_WHITE){
+	            return scoreWhite - scoreBlack;
+	         
+	        }else if(gameState == ChessGame.GAME_STATE_END_WHITE_WON
+	                || gameState == ChessGame.GAME_STATE_END_BLACK_WON){
+	            return Integer.MIN_VALUE + 1;
+	         
+	        }else{
+	            throw new IllegalStateException("unknown game state: "+gameState);
+	        }
+	    }
+	 
+	    /**
+	     * get the evaluation score for the specified piece type
+	     * @param type - one of Piece.TYPE_..
+	     * @return integer score
+	     */
+	    private int getScoreForPieceType(int type){
+	        switch (type) {
+	            case Piece.TYPE_BISHOP: return 30;
+	            case Piece.TYPE_KING: return 99999;
+	            case Piece.TYPE_KNIGHT: return 30;
+	            case Piece.TYPE_PAWN: return 10;
+	            case Piece.TYPE_QUEEN: return 90;
+	            case Piece.TYPE_ROOK: return 50;
+	            default: throw new IllegalArgumentException("unknown piece type: "+type);
+	        }
+	    } 
+}
---
+++ src/Data/Move.java
@@ -0,0 +1,22 @@
+package Data;
+
+public class Move {
+	public int sourceRow;
+	public int sourceColumn;
+	public int targetRow;
+	public int targetColumn;
+	
+	public int score;
+	public Piece capturedPiece;
+	
+	public Move(int sourceRow, int sourceColumn, int targetRow, int targetColumn) {
+		this.sourceRow = sourceRow;
+		this.sourceColumn = sourceColumn;
+		this.targetRow = targetRow;
+		this.targetColumn = targetColumn;
+	}
+	
+	public Move clone(){
+		return new Move(sourceRow,sourceColumn,targetRow,targetColumn);
+	}
+}
